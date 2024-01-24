1f585867627f261c7141977f9c7698ebc32badfa
Kriptos23 <113576598+Kriptos23@users.noreply.github.com>
2024-01-23 20:57:53 -0800

    Just starting

---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story
+{
+  String storyTitle;
+  String choice1;
+  String choice2;
+  String choice3;
+  String choice4;
+
+
+  Story(this.storyTitle, this.choice1, this.choice2, this.choice3, this.choice4);
+}
